0116dd020e98bebfcf9b836b2077506203a06bce
tonmoy <tonmoyth13@gmail.com>
2025-05-13 21:54:09 +0600

    connected mongodb

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
+require('dotenv').config()
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kqyf4iv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/',(req,res) => {
     res.send('coffee comming soon..')
